5e2ccb555f3995be9ebff7103b43612fc212aefb
Daniel Uranga <duranga@dokkogroup.com.ar>
2015-10-22 15:11:32 -0300

    Element's BitmapData cache

--- Source/com/daniel/gafplayer/Atlas.hx
+++ Source/com/daniel/gafplayer/Atlas.hx
@@ -11,10 +11,15 @@ import openfl.Assets;
 
 class Atlas {
 
+	static var bmpDataCache : Map<Int, Map<Int, BitmapData>>;
+
 	public var elements : Map<Int, BitmapData>;
 	public var elementsData : Map<Int, Element>;
 
 	public function new (from : TagDefineAtlas) {
+		if (bmpDataCache==null) {
+			bmpDataCache = new Map<Int, Map<Int, BitmapData>>();
+		}
 		elements = new Map<Int, BitmapData>();
 		elementsData = new Map<Int, Element>();
 		var sources = new Map<Int, BitmapData>();
@@ -25,14 +30,22 @@ class Atlas {
 			}
 		}
 		for (element in from.elements) {
-			var originBmp = sources[element.atlasIndex];
-			var e = new BitmapData(Std.int(element.width), Std.int(element.height)/*, false*/);
-			e.copyPixels(
-				originBmp,
-				new Rectangle(element.origin.x, element.origin.y, element.width, element.height),
-				new Point(0, 0)
-			);
-			this.elements[element.elemenetAtlasIndex] = e;
+			var bmpData : BitmapData = null;
+			if (bmpDataCache[element.atlasIndex]!=null) {
+				bmpData = bmpDataCache[element.atlasIndex][element.elemenetAtlasIndex];
+			} else {
+				bmpDataCache[element.atlasIndex] = new Map<Int, BitmapData>();
+			}
+			if (bmpData==null) {
+				bmpData = new BitmapData(Std.int(element.width), Std.int(element.height));
+				bmpData.copyPixels(
+					sources[element.atlasIndex],
+					new Rectangle(element.origin.x, element.origin.y, element.width, element.height),
+					new Point(0, 0)
+				);
+				bmpDataCache[element.atlasIndex][element.elemenetAtlasIndex] = bmpData;
+			}
+			this.elements[element.elemenetAtlasIndex] = bmpData;
 			this.elementsData[element.elemenetAtlasIndex] = element;
 		}
 	}
