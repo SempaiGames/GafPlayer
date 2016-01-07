c11ecb3762a57bd0dd3d59650eedae0b6c93200d
Daniel Uranga <duranga@dokkogroup.com.ar>
2016-01-07 15:03:05 -0300

    ParserResult class shouldn't contain Tilesheets logic code

--- com/sempaigames/gafplayer/ParserResult.hx
+++ com/sempaigames/gafplayer/ParserResult.hx
@@ -6,33 +6,16 @@ import com.sempaigames.gafplayer.tags.TagDefineAtlas;
 import flash.geom.Point;
 import flash.geom.Rectangle;
 import openfl.Assets;
-import openfl.display.Tilesheet;
 
 class ParserResult {
 
 	public var header : Header;
 	public var tags : Array<Tag>;
-	public var tilesheets : Array<Tilesheet>;
 
 	public function new (header : Header, tags : Array<Tag>) {
 
 		this.header = header;
 		this.tags = tags;
-		this.tilesheets = [];
-
-		var tagDefineAtlas = getTagsByType(TagDefineAtlas)[0];
-		for (atlas in tagDefineAtlas.atlases) {
-			var bmp = Assets.getBitmapData(Parser.assetsPrefix + atlas.sources[0].fileName, false);
-			var tilesheet = new Tilesheet(bmp);
-			tilesheets.push(tilesheet);
-		}
-
-		for (element in tagDefineAtlas.elements) {
-			tilesheets[element.atlasIndex-1].addTileRect(
-				new Rectangle(element.origin.x, element.origin.y, element.width, element.height),
-				new Point(element.pivot.x, element.pivot.y)
-			);
-		}
 
 	}
 
